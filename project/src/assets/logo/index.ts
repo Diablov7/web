1c2106ca2375f61684ee1daf0be71e6eb6aa0224
Diablov7 <diablov2021@gmail.com>
2025-09-13 19:23:43 -0300

    Delete project directory

--- project/src/assets/logo/index.ts
+++
@@ -1,4 +0,0 @@
-// Export logo paths
-export const LOGO_PATH = '/src/assets/logo/logo.png';
-export const LOGO_DARK_PATH = '/src/assets/logo/logo-dark.png';
-export const LOGO_LIGHT_PATH = '/src/assets/logo/logo-light.png';
--- project/src/components/BackgroundCubes.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function BackgroundCubes() {
-  return (
-    <div className="fixed inset-0 -z-10 bg-black" />
-  );
-} 
